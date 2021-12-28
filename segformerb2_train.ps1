a8dff8be3fa56c3152007ff593659559a26d625f
knight <you@example.com>
2021-12-28 17:40:53 +0800

    update

--- segformerb2_train.ps1
+++ segformerb2_train.ps1
@@ -1,1 +1,1 @@
-python train.py --config configs/segformer/segformer_b2_normal_seg.yml --do_eval --use_vdl --save_interval 500 --save_dir output
+python train.py --config configs/segformer/segformer_b2_normal_seg.yml --do_eval --use_vdl --save_interval 100 --save_dir output
